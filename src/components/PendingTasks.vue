1c0f98f53c614a7498c23807169ff47bab6ac331
Mutai Mwiti <mutaimwiti40@gmail.com>
2017-11-21 10:54:03 +0300

    Enable view pending tasks

--- src/components/PendingTasks.vue
+++ src/components/PendingTasks.vue
@@ -1,14 +1,43 @@
 <template>
     <div>
-        <h3>Pending tasks</h3>
-        <p>
-            Pending tasks appear here
-        </p>
+        <h1>Pending tasks</h1>
+        <template v-if="tasks.length">
+            <h3>Pending tasks</h3>
+            <table>
+                <thead>
+                <tr>
+                    <th>Name</th>
+                    <th colspan="2">Action</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr v-for="task in tasks">
+                    <td>{{task.name}}</td>
+                    <td>
+                        <button v-on:click.prevent="finishTask(task)">Done</button>
+                        <button v-on:click.prevent="removeTask(task)">Remove</button>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+        </template>
+        <template v-else>
+            You don't have any pending tasks!
+        </template>
     </div>
 </template>
 
 <script>
+    import store from '../store/store'
+
     export default {
-        name: 'PendingTasks'
+
+        name: 'PendingTasks',
+
+        computed: {
+            tasks(){
+                return store.state.tasks
+            }
+        }
     }
 </script>
